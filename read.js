9929a892af249cf1edaa563530bd433e6dc1c7c3
Sarumathi <lekha.s91@gmail.com>
2018-05-30 20:25:21 -0700

    Update read.js

--- read.js
+++ read.js
@@ -1,29 +1,142 @@
-//Retrieve task names and their due dates of project: P4 which has some pending tasks
+conn = new Mongo();
+db = conn.getDB("pid");
 
-result=db.projects.find({project:"P4"},{"user.tasks.task_name":1,"user.tasks.due_on":1,_id:0});
-print('Result for "retrieve task names and their due dates of project: P4 which has some pending tasks"');
+//Retrieving email IDs of users working under a particular project - 86
+result=db.projects.find({"project_id": 86},{_id:0, "user.email":1});
+
+print('Result for "Listing the email IDs of employees working under a particular project - 86"');
 
 while ( result.hasNext() ) {
    printjson( result.next() );
 }
 
+//Retrieving users associated with a particular project
 
- //retrieve project names which has a particular section: QA
+result=db.projects.find({project_id: 52},{_id:0,"user.user_name":1});
 
-result=db.projects.find({"section.sectionName": "qa"},{"project":1,_id:0});
-print('Result for "retrieve project names which hs a particular section: QA"');
+print('Result for "Users working for project with unique ID : 52"');
 
 while ( result.hasNext() ) {
-   printjson( result.next() );
-} 
+    printjson( result.next() );
+ }
 
- // Retrieve list of tasks of projects under workspace 1
+//Retrieving names of HR representatives of various projects
 
- result=db.projects.find({workspaceID:1},{"project":1,"user.tasks.task_name":1,_id:0});
- print('Result for "retrieve list of tasks of projects with under workspace 1"');
+result=db.projects.find({"section.section_name": "HR"},{_id:0,"user.user_name":1});
 
-// Using the code from the "Write Scripts for the mongo Shell" tutorial from doc.mongodb.org
+print('Result for "HR representatives in various projects"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+//Retrieving names of people working uner the sofware department and their associated task information
+
+result=db.projects.find({"section.section_name": "Software"},{_id:0,"user.task":1,"user.user_name":1});
+
+print('Result for "Employees working under the software department with their pending tasks"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+//Retrieving various projects listed under a particular workspace ID - along with the departments/sections
+
+result=db.projects.find({"workspace_id": 68},{_id:0,"project_id":1, "section":1});
+
+print('Result for "Various projects listed under a particular workspace ID: 68 and their departments"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+//Retrieving information relevant to a particular employee ID
+
+result=db.projects.find({"user.user_id": 1660,"section.section_name" : "Software"},{});
+
+print('Result for "Information relevant to a particular employee"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+//Retrieving tasks which are due - under a particular project
+
+result=db.projects.find({"project_id": 38},{_id:0,"user.task.task_name":1,"user.task.due_on":1});
+
+print('Result for "Pending tasks under a particular project - 38"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving email IDs of users working under a particular project - 49
+result=db.projects.find({"project_id": 49},{_id:0, "user.email":1});
+
+print('Result for "Listing the email IDs of employees working under a particular project - 49 "');
+
 while ( result.hasNext() ) {
    printjson( result.next() );
 }
 
+//Retrieving users associated with a particular project
+
+result=db.projects.find({project_id: 25},{_id:0,"user.user_name":1});
+
+print('Result for "Users working for project with unique ID : 25"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving names of employees working under the Finance section/department
+
+result=db.projects.find({"section.section_name": "Finance"},{_id:0,"user.user_name":1});
+
+print('Result for "Finance Department Employee List"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving names of people working in Testing and their associated task information
+
+result=db.projects.find({"section.section_name": "Testing"},{_id:0,"user.task":1,"user.user_name":1});
+
+print('Result for "Employees working under the software department with their pending tasks"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving various projects listed under a particular workspace ID - along with the departments/sections
+
+result=db.projects.find({"workspace_id": 43},{_id:0,"project_id":1, "section":1});
+
+print('Result for "Various projects listed under a particular workspace ID: 43 and their departments"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving tasks which are due - under a particular project
+
+result=db.projects.find({"project_id": 47},{_id:0,"user.task.task_name":1,"user.task.due_on":1});
+
+print('Result for "Pending tasks under a particular project - 47"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ //Retrieving information relevant to a particular employee ID working in a particular project
+
+result=db.projects.find({"user.user_id": 598,"project_id" : 47},{});
+
+print('Result for "Information relevant to a particular employee"');
+
+while ( result.hasNext() ) {
+    printjson( result.next() );
+ }
+
+ 
